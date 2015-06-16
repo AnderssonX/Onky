1c423ea4de2a6603563ea98713c3a7a7d8bb01b9
timearchitect <alrik_he@hotmail.com>
2015-06-17 01:20:37 +0200

    test commit

--- mobile/src/main/java/mattias/andersson/onky/CONSTANTS.java
+++ mobile/src/main/java/mattias/andersson/onky/CONSTANTS.java
@@ -6,5 +6,5 @@ import com.firebase.client.Firebase;
  * Created by Mattias on 2015-06-16.
  */
 public class CONSTANTS {
-    public static Firebase fbRef = new Firebase("https://blinding-inferno-6351.firebaseio.com/");
+    public static final Firebase fbRef = new Firebase("https://blinding-inferno-6351.firebaseio.com/");
 }
