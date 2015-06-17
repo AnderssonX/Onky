977a1c628949e1df07416b9a1223f1369f6a5696
Mattias Andersson <mattias8012@gmail.com>
2015-06-17 15:43:14 +0200

    Added design to login and signup fragments. Added game fragment App now checks firebase to see if user exists before logging in.

--- mobile/src/main/java/mattias/andersson/onky/LoginFragment.java
+++ mobile/src/main/java/mattias/andersson/onky/LoginFragment.java
@@ -1,21 +1,35 @@
 package mattias.andersson.onky;
 
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment implements View.OnClickListener {
-private Button createUser;
+    private Button createUser;
+    private FragmentManager fm;
+    private Button play;
+    private FragmentTransaction ft;
+    private boolean match;
+    private EditText eTUser;
+    private EditText eTPW;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -26,27 +40,88 @@ private Button createUser;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-final View view = inflater.inflate(R.layout.fragment_main, container, false);
+        final View view = inflater.inflate(R.layout.fragment_main, container, false);
         createUser = (Button) view.findViewById(R.id.loginSignup);
         createUser.setOnClickListener(this);
+        eTUser = (EditText) view.findViewById(R.id.loginUser);
+        eTPW = (EditText) view.findViewById(R.id.loginPw);
+        play = (Button) view.findViewById(R.id.loginLogin);
+        play.setOnClickListener(this);
+
         return view;
 
     }
 
 
     @Override
-    public void onClick(View view) {
+    public void onClick(final View view) {
         switch (view.getId()) {
 
             case R.id.loginSignup:
 
-                FragmentManager fm;
+
                 fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
+                ft = fm.beginTransaction();
                 ft.replace(R.id.container, new SignupFragment());
                 ft.commit();
 
                 break;
+
+            case R.id.loginLogin:
+
+                final Firebase fbCheck = CONSTANTS.fbRef;
+
+                fbCheck.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Iterable<DataSnapshot> fbUserCheck = dataSnapshot.getChildren();
+                        for (DataSnapshot fbUC : fbUserCheck) {
+                            //  Log.i("check", " current dataSnapshot is " + dataSnapshot.getRef());
+                            Log.i("gbuc get key ", fbUC.getKey());
+                            Log.i("gbuc get ref ", fbUC.getRef().toString());
+                            Log.i("gbuc get value ", fbUC.getValue().toString());
+
+
+                            if (eTUser.getText().toString().toLowerCase().equals(fbUC.getKey().toString())) {
+                                Log.i("check", " Match? = " + fbUC.getKey());
+
+
+                                match = true;
+                                break;
+                            }
+                            Log.i("check", " no match!");
+                        }
+                        if (match == true) {
+                            Toast toat = Toast.makeText(view.getContext(), "Logged in", Toast.LENGTH_SHORT);
+                            toat.show();
+                            //Firebase fb = new Firebase("https://blinding-inferno-6351.firebaseio.com/").child("users/" + newUser.getUsername().toString().toLowerCase());
+
+                            fm = getFragmentManager();
+                            ft = fm.beginTransaction();
+                            ft.replace(R.id.container, new GameFragment());
+                            ft.commit();
+                        }
+                        if (match == false) {
+                            Toast toat = Toast.makeText(view.getContext(), "Sorry no such user or wrong password", Toast.LENGTH_SHORT);
+                            toat.show();
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+
+                    }
+
+                });
+                break;
         }
     }
 }
+
+
+
+
+
+
