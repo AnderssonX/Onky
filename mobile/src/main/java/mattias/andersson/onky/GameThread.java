1b7b20f16b1bb83cd49996dbf250717662c6ac3e
timearchitect <alrik_he@hotmail.com>
2015-07-15 12:43:25 +0200

    canvas cleanup

--- mobile/src/main/java/mattias/andersson/onky/GameThread.java
+++ mobile/src/main/java/mattias/andersson/onky/GameThread.java
@@ -18,69 +18,41 @@ public class GameThread extends Thread {
 
 
     public GameThread(GameView view) {
-
         this.view = view;
-
     }
 
 
 
     public void setRunning(boolean run) {
-
         running = run;
-
     }
 
 
 
     @Override
-
     public void run() {
-
-        long ticksPS = 1000 / FPS;
-
-        long startTime;
-
-        long sleepTime;
-
+        long startTime, sleepTime,ticksPS = 1000 / FPS;;
         while (running) {
-
             Canvas c = null;
-
             startTime = System.currentTimeMillis();
-
             try {
-
                 c = view.getHolder().lockCanvas();
-
                 synchronized (view.getHolder()) {
-
                     view.draw(c);
-
                 }
 
             } finally {
-
                 if (c != null) {
-
                     view.getHolder().unlockCanvasAndPost(c);
-
                 }
-
             }
-
             sleepTime = ticksPS-(System.currentTimeMillis() - startTime);
 
             try {
-
                 if (sleepTime > 0)
-
                     sleep(sleepTime);
-
                 else
-
                     sleep(10);
-
             } catch (Exception e) {}
 
         }
