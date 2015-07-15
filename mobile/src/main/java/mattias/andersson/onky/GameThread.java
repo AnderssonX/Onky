0f8f199461e1a6d78c270e897167070b8e848b23
timearchitect <alrik_he@hotmail.com>
2015-07-15 13:20:03 +0200

    Revert "canvas cleanup"
    This reverts commit 1b7b20f16b1bb83cd49996dbf250717662c6ac3e.
    
    Conflicts:
    	mobile/mobile.iml
    	mobile/src/main/java/mattias/andersson/onky/GameThread.java
    	mobile/src/main/java/mattias/andersson/onky/GameView.java

--- mobile/src/main/java/mattias/andersson/onky/GameThread.java
+++ mobile/src/main/java/mattias/andersson/onky/GameThread.java
@@ -15,9 +15,24 @@ public class GameThread extends Thread {
 
 
     public GameThread(GameView view) {
+<<<<<<< HEAD
+=======
 
         this.view = view;
 
+    }
+
+>>>>>>> parent of 1b7b20f... canvas cleanup
+
+        this.view = view;
+
+<<<<<<< HEAD
+=======
+    public void setRunning(boolean run) {
+
+        running = run;
+
+>>>>>>> parent of 1b7b20f... canvas cleanup
     }
     public void setRunning(boolean run) {
 
