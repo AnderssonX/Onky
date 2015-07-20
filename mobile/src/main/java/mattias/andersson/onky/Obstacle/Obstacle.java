3441f3fedaa0008c074f2c77f5230d47d73f738b
Mattias Andersson <mattias8012@gmail.com>
2015-07-20 18:09:37 +0200

    Started on obstacle class.

--- mobile/src/main/java/mattias/andersson/onky/Obstacle/Obstacle.java
+++ mobile/src/main/java/mattias/andersson/onky/Obstacle/Obstacle.java
@@ -7,12 +7,35 @@ import java.util.Vector;
  * Created by Alrik on 2015-07-20.
  */
 public abstract class Obstacle {
+    public Vector vectorXy;
+    public Vector vectorWh;
+    boolean dead, regenerating;
 
-    boolean dead,regenerating;
-
-    public Obstacle(){
+    public Obstacle() {
 
 
     }
 
+    public Obstacle(Vector vectorXy, Vector vectorWh) {
+
+        this.vectorXy = vectorXy;
+        this.vectorWh = vectorWh;
+
+    }
+
+    public Vector getVectorXy() {
+        return vectorXy;
+    }
+
+    public void setVectorXy(Vector vectorXy) {
+        this.vectorXy = vectorXy;
+    }
+
+    public Vector getVectorWh() {
+        return vectorWh;
+    }
+
+    public void setVectorWh(Vector vectorWh) {
+        this.vectorWh = vectorWh;
+    }
 }
