0b80da77c18d8f58028766f870dd442502bfcc15
timearchitect <alrik_he@hotmail.com>
2015-07-20 15:13:59 +0200

    class

---
+++ mobile/src/main/java/mattias/andersson/onky/Obstacle/Obstacle.java
@@ -0,0 +1,18 @@
+package mattias.andersson.onky.Obstacle;
+
+
+import java.util.Vector;
+
+/**
+ * Created by Alrik on 2015-07-20.
+ */
+public abstract class Obstacle {
+
+    boolean dead,regenerating;
+
+    public Obstacle(){
+
+
+    }
+
+}
