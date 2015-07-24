c67fdc45961da348ac6ffeb601ef41ea99bc0162
timearchitect <alrik_he@hotmail.com>
2015-07-24 14:53:08 +0200

    boxes particles class

--- mobile/src/main/java/mattias/andersson/onky/Obstacle/IronBox.java
+++ mobile/src/main/java/mattias/andersson/onky/Obstacle/IronBox.java
@@ -1,26 +1,19 @@
 package mattias.andersson.onky.Obstacle;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
-
-import mattias.andersson.onky.GameView;
-import mattias.andersson.onky.R;
 
 /**
  * Created by Alrik on 2015-07-23.
  */
-public class Box extends  Obstacle {
+public class IronBox extends  Obstacle {
    int defaultHealth,health=defaultHealth;
     Drawable image;
   //  Bitmap bMap = BitmapFactory.decodeFile("main/assets/woodenBox.png");
    // Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.woodenBox.png);
-    public Box(Point2D coord, Point2D Size) {
+    public IronBox(Point2D coord, Point2D Size) {
        super(coord, Size);
-    color.setARGB(255,180, 140, 50);
+    color.setARGB(255,200, 200, 200);
      //  image=C:\Users\Alrik\AndroidStudioProjects\Onky\mobile\src\main\assets\woodenBox.png
 
     //   Drawable d = Drawable.createFromStream(getAssets().open("woodenBox.png"), null);
