e1ff422b962f62e6b2090fae121bbf40af4665d5
Hussam <hussamali.92@gmail.com>
2015-08-06 14:15:12 +0200

    Store  with clickable images

--- mobile/src/main/java/mattias/andersson/onky/Store.java
+++ mobile/src/main/java/mattias/andersson/onky/Store.java
@@ -7,12 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
-import java.util.ArrayList;
-
 import mattias.andersson.onky.helper.CONSTANTS;
-import mattias.andersson.onky.helper.StockMarket;
 import mattias.andersson.onky.helper.Upgrade;
 
 
@@ -21,20 +19,32 @@ import mattias.andersson.onky.helper.Upgrade;
  */
 public class Store extends Fragment implements View.OnClickListener {
 
+    ImageButton image1, image2,image3, image4,image5, image6;
+    int slowUpgrade, teleportUpgrade, shootUpgrade, invinceUpgrade, coinUpgrade, magnetUpgarde;
 
 
     public Store() {
         // Required empty public constructor
-
-
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_store, container, false);
-        //ImageButton laserButton = (ImageButton)view.findViewById(R.id.imageButton1);
+        image1 = (ImageButton) view.findViewById(R.id.imageButton1);
+        image1.setOnClickListener(this);
+        image2 = (ImageButton) view.findViewById(R.id.imageButton2);
+        image2.setOnClickListener(this);
+        image3 = (ImageButton) view.findViewById(R.id.imageButton3);
+        image3.setOnClickListener(this);
+        image4 = (ImageButton) view.findViewById(R.id.imageButton4);
+        image4.setOnClickListener(this);
+        image5 = (ImageButton) view.findViewById(R.id.imageButton5);
+        image5.setOnClickListener(this);
+        image6 = (ImageButton) view.findViewById(R.id.imageButton6);
+        image6.setOnClickListener(this);
         CONSTANTS. upgrades.add(new Upgrade("slow", 1));
         CONSTANTS. upgrades.add(new Upgrade("laser",1));
         CONSTANTS. upgrades.add(new Upgrade("invis",4));
@@ -43,12 +53,61 @@ public class Store extends Fragment implements View.OnClickListener {
         CONSTANTS. upgrades.add(new Upgrade("ONKY",1));
         CONSTANTS. upgrades.add(new Upgrade("bonus",3));
 
+        //ImageButton laserButton = (ImageButton)view.findViewById(R.id.imageButton1);
+
+
+
+
+
+
         return view;
     }
 
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageButton1:
+                image1.setBackgroundResource(R.drawable.achievementiconclicked);
+                slowUpgrade++;
+                if(slowUpgrade==3)
+                    Log.i("clicked", "Hope you´re happy with your purchase");
+                break;
 
-    }
-}
+            case R.id.imageButton2:
+                image2.setBackgroundResource(R.drawable.achievementiconclicked);
+                Log.i("clicked", "Hope you´re happy with your purchase");
+
+
+                break;
+            case R.id.imageButton3:
+                image3.setBackgroundResource(R.drawable.achievementiconclicked);
+                Log.i("clicked", "Hope you´re happy with your purchase");
+
+
+                break;
+            case R.id.imageButton4:
+                image4.setBackgroundResource(R.drawable.achievementiconclicked);
+                Log.i("clicked", "Hope you´re happy with your purchase");
+
+
+                break;
+            case R.id.imageButton5:
+                image5.setBackgroundResource(R.drawable.achievementiconclicked);
+                Log.i("clicked", "Hope you´re happy with your purchase");
+
+
+                break;
+            case R.id.imageButton6:
+                image6.setBackgroundResource(R.drawable.achievementiconclicked);
+                Log.i("clicked", "Hope you´re happy with your purchase");
+
+
+                break;
+
+
+
+
+
+        }
+    }}
