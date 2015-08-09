949b66a7683040a3fe96695bd45f3089af300935
Hussam <hussamali.92@gmail.com>
2015-08-09 15:50:15 +0200

    here are the missing files

---
+++ mobile/src/main/java/mattias/andersson/onky/Highscore.java
@@ -0,0 +1,30 @@
+package mattias.andersson.onky;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Highscore extends Fragment {
+
+
+    public Highscore() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_highscore, container, false);
+    }
+
+
+}
---
+++ mobile/src/main/java/mattias/andersson/onky/MainMenu.java
@@ -0,0 +1,95 @@
+package mattias.andersson.onky;
+
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import mattias.andersson.onky.helper.CONSTANTS;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainMenu extends Fragment implements View.OnClickListener {
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Button play;
+    private Button highscore;
+    private Button options;
+    private Button store;
+
+
+    public MainMenu() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+
+        play = (Button) view.findViewById(R.id.Play);
+        highscore = (Button) view.findViewById(R.id.Highscore);
+        options = (Button) view.findViewById(R.id.Options);
+        store = (Button) view.findViewById(R.id.Store);
+
+        play.setOnClickListener(this);
+        highscore.setOnClickListener(this);
+        options.setOnClickListener(this);
+        store.setOnClickListener(this);
+
+
+        return view;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Play:
+                Log.i("offline", "Store");
+                fm = getFragmentManager();
+                ft = fm.beginTransaction();
+                ft.replace(R.id.container, new GameFragment());
+                ft.commit();
+
+                break;
+            case R.id.Highscore:
+                Log.i("offline", "Highscore");
+                fm = getFragmentManager();
+                ft = fm.beginTransaction();
+                ft.replace(R.id.container, new LoginFragment());
+                ft.commit();
+
+                break;
+            case R.id.Options:
+                Log.i("offline", "Highscore");
+                fm = getFragmentManager();
+                ft = fm.beginTransaction();
+                ft.replace(R.id.container, new Options());
+                ft.commit();
+
+                break;
+            case R.id.Store:
+                Log.i("offline", "Highscore");
+                fm = getFragmentManager();
+                ft = fm.beginTransaction();
+                ft.replace(R.id.container, new Store());
+                ft.commit();
+
+                break;
+    }
+
+
+
+}}
